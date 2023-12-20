17082b18b8c454a3dfb6bd3b6d568881a8b68ba9
JOTARO <380686356@qq.com>
2023-12-20 13:32:31 +0800

    first commit

---
+++ src/views/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{ showText }}</div>
+  <button>创建钱包</button>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const showText = ref("Hello Web3！");
+</script>
+
+<style lang="scss">
+div {
+  color: red;
+}
+</style>
