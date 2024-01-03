7da892737ace6a42b166c00904e3424a56c4d1df
JOTARO <380686356@qq.com>
2024-01-03 13:38:32 +0800

    keyStore获取私钥

--- src/views/AccountSystem.vue
+++ src/views/AccountSystem.vue
@@ -1,15 +1,82 @@
 <template>
   <h1>助记词</h1>
-  <p>panic double van adapt rookie lock circle you violin odor island strong</p>
+  <p>{{ mnemonciSave }}</p>
+
+  <h1>路径</h1>
+  <p>m/44'/60/0'/0'/0/0</p>
+
+  <h1>账号地址</h1>
+  <p>0x1e1c929da25b99f2a7c3a4f4c8de6151719e0678</p>
+
+  <h1>账号私钥</h1>
+  <p>840ef006deb7e8ad84b2166e5598d4691786ff59d505650a4043d6fab6cd7af8</p>
 </template>
 
 <script setup>
 import { ref } from "vue";
-import {generateMnemonic} from "bip39"
+import { generateMnemonic, mnemonicToSeed } from "bip39";
+import ethwallet, { hdkey } from "ethereumjs-wallet";
+import { onMounted } from "vue";
+import Web3 from "web3";
 
-
+onMounted(() => {
+  genMnemonic();
+});
 //创建助记词
-const mnemonci = generateMnemonic();
+// const mnemonci = generateMnemonic();
+// console.log(mnemonci);
 
-console.log(mnemonci);
+const mnemonciSave = ref(
+  "panic double van adapt rookie lock circle you violin odor island strong"
+);
+
+//根据助记词 生成 秘钥对
+const genMnemonic = async () => {
+  //获取种子
+  const seed = await mnemonicToSeed(mnemonciSave.value);
+  console.log(seed);
+  const hdWallet = hdkey.fromMasterSeed(seed);
+  //m/purpse'/coin_type'/account'/change/address_index
+  const keypair = hdWallet.derivePath("m/44'/60/0'/0'/0/0");
+  console.log(keypair);
+
+  //获取私钥
+  //根据秘钥对 获取钱包对象
+  const wallet = keypair.getWallet();
+  console.log(wallet);
+
+  //根据钱包对象获取钱包地址
+  const lowerCaseAddress = wallet.getAddressString();
+  console.log("lowerCaseAddress", lowerCaseAddress);
+  //根据钱包对象获取钱包校验地址
+  const checkAddress = wallet.getChecksumAddressString();
+  console.log("checkAddress", checkAddress);
+  //获取私钥
+  const privateKey = wallet.getPrivateKey().toString("hex");
+  const privateKeyFormat = wallet.getPrivateKey();
+
+  console.log("privateKey", privateKey);
+
+  // 根据私钥+密码 获取 keystore
+  //1.web3js
+  let web3Ins = new Web3(
+    Web3.givenProvider ||
+      "wss://goerli.infura.io/ws/v3/c0b3b16253fb4528962978ae6b6ed58b"
+  );
+  const keyStore = await web3Ins.eth.accounts.encrypt(
+    privateKeyFormat,
+    "111111"
+  );
+  console.warn(keyStore);
+
+  //2.wallet对象 + 密码 获取keysotre
+  // const walletKeyStore = await wallet.toV3("111111");
+  // console.warn(walletKeyStore);
+
+  //通过keyStore获取私钥
+  const res = await web3Ins.eth.accounts.decrypt(keyStore, "111111");
+  console.log(res);
+
+  // const res2 = ethwallet.fromV3(keyStore);
+};
 </script>
